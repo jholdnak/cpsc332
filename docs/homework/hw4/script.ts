5c892c8bbc0048f1193f0ab58d4972dc871baa38
jholdnak <jholdnak@mailbox.org>
2022-10-21 20:50:23 -0700

    Stuff

--- docs/homework/hw4/script.ts
+++ docs/homework/hw4/script.ts
@@ -10,6 +10,9 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 function domLoaded() {
     const startButton = document.getElementById('start-btn')
     const nextButton = document.getElementById('next-btn')
+    const previousButton = document.getElementById('previous-btn')
+    const additionalInfoElement = document.getElementById('additional-info')
+    const additionalInfoContainerElement = document.getElementById('additional-info-container')
     const optionsContainerElement = document.getElementById('options')
     const questionContainerElement = document.getElementById('question-container')
     const questionElement = document.getElementById('question')
@@ -18,6 +21,51 @@ function domLoaded() {
     const shuffleCardsOption = document.getElementById('shuffleCards')
     const timedSessionOption = document.getElementById('timedSession')
     const removeCorrectFromDeckOption = document.getElementById('removeCorrectFromDeck')
+    //Additional features
+    
+    // TIMER
+
+    //The current count of the timer is zero
+    let elapsedTimeElement = document.getElementById('elapsedTime')
+    let timer = 0;
+    let timerInterval;
+    //One of the id functions of the three time boxes has been assigned a global constant.
+    let ms = document.getElementById('ms');
+    let second = document.getElementById('second');
+    let minute = document.getElementById('minute');
+
+    function start() {
+        stop();
+        //All calculations are stored in "timerInterval".
+        //Here "setInterval" is used and 1000/60 seconds is set.
+        //As a result, this calculation will be updated every 1 millisecond
+        timerInterval = setInterval(function() {
+            //Now the value of 'timer' has been set
+            //We know x + = y => x = x + y
+            timer += 1/60;
+            //Now the value of 'milliseconds' has been set. It is also stored in "msVal".
+            let msVal = Math.floor((timer - Math.floor(timer))*100);
+            //The value of 'seconds' has been determined in the same way.
+            let secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+            //The value of 'minute' has been determined.
+            let minuteVal = Math.floor(timer/60);
+
+            //With the help of 'innerHTML' all the information is displayed in the webpage
+            //The condition here is that when the value of time is less than 10 then a 0 will be added before that time.
+            // For milliseconds
+            ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
+            //For seconds
+            second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
+            // For Minute
+            minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
+        }, 1000/60);
+    }
+
+    //Now you have to activate the stop button
+    //The clearInterval() function in javascript clears the interval which has been set by setInterval() function before that.
+    function stop() {
+        clearInterval(timerInterval);
+    }
 
     let shuffledQuestions, currentQuestionIndex
     //using the domLoaded function because the buttons need to be rendered before we can
@@ -27,17 +75,23 @@ function domLoaded() {
         currentQuestionIndex++
         setNextQuestion()
     })
+    previousButton.addEventListener('click', () => {
+        currentQuestionIndex--
+        setNextQuestion()
+    })
 
     function startGame() {
         startButton.classList.add('hide')
+        additionalInfoContainerElement.classList.remove('hide')
         optionsContainerElement.style.display = 'none'
         shuffledQuestions = questions.sort(() => Math.random() - .5)
+        if (timedSessionOption.checked) {
+            start()
+            elapsedTimeElement.classList.remove('hide')
+        }
         currentQuestionIndex = 0
         questionContainerElement.classList.remove('hide')
         setNextQuestion()
-
-        // The part I'm working on
-        if (shuffleCardsOption.checked)
     }
 
     function setNextQuestion() {
@@ -45,7 +99,10 @@ function domLoaded() {
         showQuestion(shuffledQuestions[currentQuestionIndex])
     }
 
+    function removeCorrectFromDeck() {}
+
     function showQuestion(question) {
+        additionalInfoElement.innerText = question.details
         questionElement.innerText = question.question
         question.answers.forEach(answer => {
             const button = document.createElement('button')
@@ -76,6 +133,7 @@ function domLoaded() {
         })
         if (shuffledQuestions.length > currentQuestionIndex + 1) {
             nextButton.classList.remove('hide')
+            previousButton.classList.remove('hide')
         } else {
             startButton.innerText = 'Restart'
             startButton.classList.remove('hide')
@@ -99,23 +157,17 @@ function domLoaded() {
     const questions = [
         {
             question: 'This is a test question',
+            details: 'lol',
             answers: [
                 { text: 'Correct Answer', correct: true },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
                 { text: 'Incorrect Answer', correct: false },
             ]
         },
         {
             question: 'This is another test question',
+            details: '',
             answers: [
                 { text: 'Correct Answer', correct: true },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
-                { text: 'Incorrect Answer', correct: false },
                 { text: 'Incorrect Answer', correct: false },
             ]
         },
